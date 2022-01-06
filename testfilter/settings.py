06a3a4779315e57f5ead4bc78b546fa413b90433
Roman Antonov <roman@debian-BULLSEYE-live-builder-AMD64>
2022-01-06 23:56:27 +0300

    bootstrap cdn....

--- testfilter/settings.py
+++ testfilter/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0-5++4kqy-xisysxwr!i7et!^s3@8v%nt8wch6oiy3rzbm_#p&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
